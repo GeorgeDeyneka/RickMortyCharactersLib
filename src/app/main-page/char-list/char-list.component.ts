bea96e6ca1b0d0ef1318edec52a6c8f4a63aafeb
GeorgeDeyneka <zhoradeynecka@gmail.com>
2023-03-11 03:21:08 +0200

    Add fonts, reset and typography styles

--- src/app/main-page/char-list/char-list.component.ts
+++ src/app/main-page/char-list/char-list.component.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseHttpService } from 'src/app/shared/services/base-http.service';
 
+interface ICharacter {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: object;
+  location: object;
+  image: string;
+  episode: Array<any>;
+  url: string;
+  created: string;
+}
+
 @Component({
   selector: 'app-char-list',
   templateUrl: './char-list.component.html',
   styleUrls: ['./char-list.component.scss'],
 })
 export class CharListComponent implements OnInit {
-  constructor(private baseHttpService: BaseHttpService) { }
+  constructor(private baseHttpService: BaseHttpService) {}
 
-  public data: any = []
+  public data: Array<ICharacter> = [];
 
   ngOnInit(): void {
-    this.baseHttpService.getList().subscribe((data) => {
-      this.data = data;
-      console.log(this.data)
-    })
+    this.baseHttpService.getList().subscribe((data: any) => {
+      this.data = data.results;
+      console.log(this.data);
+    });
   }
 }
